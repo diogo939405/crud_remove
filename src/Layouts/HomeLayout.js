8446804505f547e26b8280551b72ceefa7a871aa
Diogo <diogo.favilla@gmail.com>
2023-10-24 10:05:29 -0300

    terça 24/10/2023

--- src/Layouts/HomeLayout.js
+++ src/Layouts/HomeLayout.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import  './HomeLayout.css'
+import './HomeLayout.css'
 
 export default function HomeLayout() {
   return (
-    <div className ='corpo' >
-        <header className='cabecalho'>
-            <img src="../imagens/logo.png" height="110px" alt='cabeçalho'></img>
-                      fefef
-        </header>
-   <body >
+    <div className='corpo' >
+      <header className='cabecalho'>
+        <div>
+        <img className='foto' src="https://rb1-condominio.com.br/extranet/wp-content/uploads/2023/08/Logo_top1-1-e1653228986853-meno.png" width="107px" height="61px" alt='cabeçalho'></img>
+        </div>
+        <nav className='menu'>
+          <span className='menuPalavra'> Principal</span>
+          <span className='menuPalavra'> Aniversariantes</span>
+          <span className='menuPalavra'> LGPD</span>
+          <span className='menuPalavra'>Controle de Acesso</span>
+          <span className='menuPalavra'> Sair</span>
+        </nav>
+      </header>
+      <body >
         <div className>
 
         </div>
-   </body>
-    <Outlet/>
+      </body>
+      <Outlet />
     </div>
   )
 }
