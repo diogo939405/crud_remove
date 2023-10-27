37ef2e9f5096d893c05db5d27b81827c47766e1f
diogo939405 <diogo.favilla@gmail.ccom>
2023-10-27 11:28:44 -0300

    26/10/2023

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-import {getdata} from "../service/service"
+import { getdata } from "../service/service"
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 export default function Home() {
 
   const [serviceData, setServiceData] = useState([]);
+  const [rowClick, setRowClick] = useState(true);
+  const [selectedProducts, setSelectedProducts] = useState(null);
   useEffect(() => {
-    getdata().then((res) => setServiceData(res.data))
-  },[]);
+    getdata().then((res) => {setServiceData(res.data);console.log('efefee',res.data)})
+  }, []);
 
-  Promise.resolve(getdata()).then(rr => console.log('EUUUU',rr))
+ 
   return (
     <div>
+      <DataTable value={serviceData} scrollable scrollHeight="70vh"
+        dataKey="id"
+        selectionMode={rowClick ? null : 'checkbox'}
+        selection={selectedProducts}
+        onSelectionChange={(e) => setSelectedProducts(e.value)}
+        tableStyle={{ minWidth: '50rem' }}>
+
+        <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
+        <Column field="name" header="Nome"></Column>
+        {/* <Column field="name" header="Name"></Column>
+        <Column field="category" header="Category"></Column>
+        <Column field="quantity" header="Quantity"></Column> */}
+      </DataTable>
       <Outlet />
     </div>
   )
--- src/service/service.js
+++ src/service/service.js
@@ -1,10 +1,15 @@
+
+import React, { useState} from 'react'
 export async function getdata() {
+    const [result, setResult] = useState([]);
     const url = "https://blue-enchanting-macaw.cyclic.cloud/diogo";
     console.log('ENTROU', url)
-    let result = {};
+    // let result = {};
     await fetch(url).then(res => {
-        result = res.json()
-        console.log('RESULT  ', result)
+        setResult(res.json)
+        
     });
+     Promise.resolve(getdata()).then((rr) =>console.log("Console do rr",rr));
+    console.log('RESULT  ',  Promise.resolve(getdata()))
     return result;
 }
