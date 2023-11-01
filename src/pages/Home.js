d61124e83651d85b30bbb381eead8d80d5d525b0
Diogo <diogo.favilla@gmail.com>
2023-11-01 12:08:23 -0300

    tamanhon e btn OK

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,84 @@
 import React, { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-import { Getdata } from "../service/service"
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+import axios from "axios"
 import './Home.css'
+import 'primeicons/primeicons.css';
 
 export default function Home() {
-
-  const [serviceData, setServiceData] = useState([{ }]);
+  const [displayButton, setDisplayButton] = useState('none')
+  const [serviceData, setServiceData] = useState([{}]);
   const [rowClick, setRowClick] = useState(true);
   const [selectedProducts, setSelectedProducts] = useState(null);
 
-  // useEffect(() => {
-  //   Getdata().then((data) =>  setServiceData(data));
-  // }, []);
-  useEffect(() => {
-    // Define the URL of the API you want to fetch data from
-    const apiUrl = 'https://blue-enchanting-macaw.cyclic.cloud/diogo';
 
-    // Fetch the data using the fetch API
+  useEffect(() => {
+    const apiUrl = 'https://blue-enchanting-macaw.cyclic.cloud/diogo';
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
-        // Update the data state with the fetched data
+
         console.log(data)
         setServiceData(data);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-  }, []); 
+  }, []);
+  const botoes = (rowdata) => {
+    return <Button onClick={() => deletarUnicoUsuario(rowdata)} icon="pi pi-times" severity="danger" aria-label="Cancel"></Button>
+  
+  }
+  const deletarUnicoUsuario = (data) => {
+    const confirma = window.confirm("Deseja apagar?");
+    if (confirma) {
+      deleteUser(data)
+    }
+  }
 
+  const deleteUser = (data) => {
+    axios.delete(`http://localhost:4000/usuarios/${data.id}`)
+      .then(res => {
+        window.location.reload()
+      })
+  }
+
+  const deletar = (ee) => {
+    const confirma = window.confirm("Deseja apagar?");
+    if (confirma) {
+      selectedProducts.forEach(element => {
+        console.log('papai', element)
+        deleteUser(element)
+
+      });
+    }
+  }
+
+  const Seleciona = (e) => {
+    setSelectedProducts(e.value)
+    setDisplayButton(e.value.length === 0 ? 'none' : 'block')
+  }
   return (
+    
     <div className='content'>
-      <DataTable value={serviceData} scrollable scrollHeight="70vh" className='tabela' 
+
+      <Button label="Apagar" severity="danger" rounded onClick={deletar} className="id" style={{ display: displayButton }}  >
+
+      </Button>
+
+      <DataTable value={serviceData} scrollable scrollHeight="70vh" className='tabela'
         dataKey="id"
         selectionMode={rowClick ? null : 'checkbox'}
         selection={selectedProducts}
-        onSelectionChange={(e) => setSelectedProducts(e.value)}
+        onSelectionChange={Seleciona}
         tableStyle={{ Width: '50rem' }}>
 
         <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
         <Column field="name" header="Nome"></Column>
-        <Column field="slug" header="Email"></Column> 
+        <Column field="slug" header="Email"></Column>
+        <Column header="Ações" body={botoes} style={{ minWidth: '2rem' }}  ></Column>
 
       </DataTable>
       <Outlet />
