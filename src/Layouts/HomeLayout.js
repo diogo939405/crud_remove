6367b6f4791474515568996c8c89d9f26977fb6f
diogo939405 <diogo.favilla@gmail.ccom>
2023-11-07 18:11:52 -0300

    teste

--- src/Layouts/HomeLayout.js
+++ src/Layouts/HomeLayout.js
@@ -12,7 +12,7 @@ export default function HomeLayout() {
         <img className='foto' src="https://rb1-condominio.com.br/extranet/wp-content/uploads/2023/08/Logo_top1-1-e1653228986853-meno.png" width="107px" height="61px" alt='cabeçalho'></img>
         <nav className='nav'>
           <ul className='menu'>
-            <li className='menuPalavra'> PRINCIPAL</li>
+            <li className='menuPalavra'> PRINCIPAL fiz a mudança aqui</li>
             <li className='menuPalavra'> ANIVERSARIANTES</li>
             <li className='menuPalavra'> LGPD</li>
             <div id="MenuLGPD">
