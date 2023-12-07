649194d34debe64351638a123c7b0bc82ba76bda
Nilton Cesar Jacintho <nilton.jacintho@gmail.com>
2023-12-07 10:02:17 -0300

    teste

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,7 +28,7 @@ export default function Home() {
   // const router = useRouter();
 
   const apiUrl = 'https://blue-enchanting-macaw.cyclic.cloud/';
-  // const apiUrl = 'http://localhost:3010/';
+  // const apiUrl = 'http://localhost:3010/';--
 
 
   useEffect(() => {
